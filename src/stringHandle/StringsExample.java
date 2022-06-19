0915c903c93854f71a3c9702867127e33f7f508a
Mahesh <70198087+maheshmaersk@users.noreply.github.com>
2022-06-19 09:10:45 +0530

    Strings Example and Sample

---
+++ src/stringHandle/StringsExample.java
@@ -0,0 +1,60 @@
+package stringHandle;
+
+import java.util.Locale;
+
+public class StringsExample {
+
+    public static void main(String[] args) {
+
+        String mango = "Mango";
+        String a = "Apple";
+        String c = "Mango";
+
+        String d = new String("Apple");
+        String e = new String("Mango");
+
+       /* if (c == e) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+        if (mango == c) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+        if (c.equals(e)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }*/
+
+
+        String abc = "Hello How are you";
+        String abc1 = "Hello how ArE yOu";
+        System.out.println(abc.trim());
+        System.out.println(abc1.equalsIgnoreCase(abc));
+
+        String[] sdf = abc.split(" ");
+        System.out.println(sdf.length);
+        System.out.println(abc.length());
+
+        System.out.println(abc.substring(0,6));
+        System.out.println(abc.toUpperCase(Locale.ROOT));
+        System.out.println(abc.toLowerCase(Locale.ROOT));
+
+        System.out.println(abc.concat(abc1));
+        System.out.println(abc+" "+(abc1));
+        System.out.println(abc.contains("How"));
+        System.out.println(abc.charAt(3));
+        System.out.println(abc.replace("How","Harsha"));
+        System.out.println(abc.replace("How ",""));
+        System.out.println(abc.startsWith("How"));
+        System.out.println(abc.endsWith("you"));
+
+
+
+    }
+}
